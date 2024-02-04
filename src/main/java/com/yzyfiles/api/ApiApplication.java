681c353d29b0dba41e5357d31ad1b40ce1d527a7
BotMertCool <68245869+BotMertCool@users.noreply.github.com>
2024-02-04 02:30:44 -0500

    index

--- src/main/java/com/yzyfiles/api/ApiApplication.java
+++ src/main/java/com/yzyfiles/api/ApiApplication.java
@@ -1,4 +1,5 @@
-package com.yzyfiles.api.controllers;
+package com.yzyfiles.api;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/yzyfiles/api/controllers/TestController.java
+++ src/main/java/com/yzyfiles/api/controllers/TestController.java
@@ -1,9 +1,11 @@
-package com.yzyfiles.api;
+package com.yzyfiles.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class TestController {
 
     @GetMapping
