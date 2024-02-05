46399d27e2f4cec3c7d38f985de0868562e583c1
BotMertCool <68245869+BotMertCool@users.noreply.github.com>
2024-02-05 17:10:59 -0500

    fix gitignore

--- src/main/java/com/yzyfiles/graphite/Graphite.java
+++ src/main/java/com/yzyfiles/graphite/Graphite.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class Graphite {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Graphite.class, args);
 	}
-
 }
